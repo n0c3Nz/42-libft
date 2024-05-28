30be45c631f6a60110411a09ad3f0ad8090e8bab
Guillermo Ortún Delicado <agapitobusiness@gmail.com>
2024-05-28 12:18:22 +0200

    Removed comments from files

--- libft.h
+++ libft.h
@@ -1,14 +1,14 @@
-/* ************************************************************************** */
-/*                                                                            */
-/*                                                        :::      ::::::::   */
-/*   libft.h                                            :+:      :+:    :+:   */
-/*                                                    +:+ +:+         +:+     */
-/*   By: dmaldona <dmaldona@student.42madrid.com    +#+  +:+       +#+        */
-/*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/07/05 11:07:00 by dmaldona          #+#    #+#             */
-/*   Updated: 2022/09/21 21:32:05 by dmaldona         ###   ########.fr       */
-/*                                                                            */
-/* ************************************************************************** */
+
+
+
+
+
+
+
+
+
+
+
 
 #ifndef LIBFT_H
 # define LIBFT_H
@@ -18,8 +18,8 @@
 # include <stdio.h>
 # include <stdint.h>
 
-/** simple functions **/
-/* characters functions */
+
+
 int		ft_atoi(const char *str);
 int		ft_isalpha(int c);
 int		ft_isascii(int c);
@@ -29,7 +29,7 @@ int		ft_isdigit(int c);
 int		ft_isalnum(int c);
 int		ft_isprint(int c);
 
-/* memory functions */
+
 int		ft_memcmp(const void *s1, const void *s2, size_t n);
 void	*ft_memchr(const void *s, int c, size_t n);
 void	*ft_calloc(size_t count, size_t size);
@@ -37,7 +37,7 @@ void	*ft_memset(void *b, int c, size_t len);
 void	*ft_memcpy(void *dst, const void *src, size_t n);
 void	*ft_memmove(void *dst, const void *src, size_t len);
 
-/* string functions */
+
 char	*ft_strrchr(const char *s, int c);
 char	*ft_strchr(const char *s, int c);
 size_t	ft_strlen(const char *s);
@@ -48,7 +48,7 @@ size_t	ft_strlcpy(char *dst, const char *src, size_t dstsize);
 char	*ft_strnstr(const char *h, const char *n, size_t len);
 int		ft_strncmp(const char *s1, const char *s2, size_t n);
 
-/** complex functions **/
+
 char	*ft_strjoin(char const *s1, char const *s2);
 char	*ft_substr(char const *s, unsigned int start, size_t len);
 char	*ft_strtrim(char const *s1, char const *set);
@@ -61,7 +61,7 @@ void	ft_putnbr_fd(int n, int fd);
 void	ft_striteri(char *s, void (*f)(unsigned int, char*));
 char	*ft_strmapi(char const *s, char (*f)(unsigned int, char));
 
-/** lists functions **/
+
 typedef struct s_list
 {
 	void			*content;
